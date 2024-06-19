0d59e0c21bc5e3cbfd7f8f27ef7b1b9ae1c69f9d
[SW5] 이수민 <tnklr1111@gmail.com>
2024-06-19 19:38:09 +0900

    fix: asyncHandler 미들웨어 코드 수정

--- src/middlewares/asyncHandler.js
+++ src/middlewares/asyncHandler.js
@@ -2,9 +2,9 @@ const asyncHandler = (requestHandler) => {
   return async (req, res, next) => {
     try {
       const result = await requestHandler(req, res);
-      if (!result) {
-        return res.status(404).json({ error: 'not found' });
-      }
+      // if (!result) {
+      //   return res.status(404).json({ error: 'not found' });
+      // }
     } catch (err) {
       next(err);
     }
